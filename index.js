c71b7d20034db731754acb42f9d6b1a0e04628f2
thepresidentm <miguelanva9@gmail.com>
2022-03-27 17:25:16 -0600

    Hola mundo

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("xd");
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || "5000";
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
+app.get("/", (req, res) => {
+    res.json({ mensaje: "Hola Mundo"});
+});
